e764c40e2e4d10e02f2ab92187e49bd37077f064
H151699 <andrewj.day@hotmail.com>
2019-12-11 01:06:20 +0100

    add delete and add functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas'
+import AddNinja from './AddNinja'
 
 class App extends Component {
   state = {
@@ -9,11 +10,56 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
+
+  /** Add ********************************************************/
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    // create ninjas to a new array
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  /** Delete ********************************************************/
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+
+    })
+  }
+
+  iClick = (e) => {
+    console.log(e.target)
+      this.setState({
+      ninjas:[
+        {name: 'jianyou', age: 22, belt: 'green'}
+      ]
+    })
+  }
+  
+  /** componentDidMount componentDidUpdate ****************************/
+  componentDidMount() {
+    console.log('component mounted')
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('component updated');
+    console.log('prevProps prevState')
+  }
+
+  /** render ********************************************************/
   render() {
     return (
       <div className="App">
-        <h1>My first React app</h1>
-        <Ninjas ninjas={this.state.ninjas}/>
+        <h1> 戴天宇 </h1>
+        <AddNinja addNinja={this.addNinja} />
+        <Ninjas deleteNinja={this.deleteNinja} iClick={this.iClick} ninjas={this.state.ninjas} />
+
+        
       </div>
     );
   }
